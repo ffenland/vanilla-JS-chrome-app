8bc5ce75a9ef3ead45d611834339fa929637ea0b
ffenland <ffenland@gmail.com>
2022-10-12 01:13:47 +0900

    clock start

--- js/clock.js
+++ js/clock.js
@@ -1,1 +1,15 @@
 const clock = document.querySelector("h2#clock");
+
+const getClock = () => {
+  const date = new Date();
+  const hours =
+    date.getHours() >= 13
+      ? String(date.getHours() - 12).padStart(2, "0")
+      : String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const second = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${second}`;
+};
+
+getClock();
+setInterval(getClock, 1000);
---
+++ js/quotes.js
@@ -0,0 +1,1 @@
+const quotes = [{ quote: "hi", author: "" }];
